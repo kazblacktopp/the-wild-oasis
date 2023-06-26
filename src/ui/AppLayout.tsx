97ec0bca17956c212b2b00918421a8737573a33f
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-26 10:28:19 +1000

    Add styles to 'main' element

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,15 +1,22 @@
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 import Header from './Header';
 import Sidebar from './Sidebar';
+
+const Main = styled.main`
+	background-color: var(--color-grey-50);
+	padding: 4rem 4.8rem 6.4rem;
+	overflow: scroll;
+`;
 
 export default function AppLayout() {
 	return (
 		<div>
 			<Header />
 			<Sidebar />
-			<main>
+			<Main>
 				<Outlet />
-			</main>
+			</Main>
 		</div>
 	);
 }
