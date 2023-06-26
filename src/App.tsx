50d9d0a09e874f5083bfc325cd883990d7a6cadc
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-26 11:22:17 +1000

    Add Bookings, Cabins, Users & Settings to router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import Dashboard from './pages/Dashboard';
 import AppLayout from './ui/AppLayout';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
 
 export default function App() {
 	return (
@@ -15,6 +19,10 @@ export default function App() {
 							element={<Navigate replace to="dashboard" />}
 						/>
 						<Route path="dashboard" element={<Dashboard />} />
+						<Route path="bookings" element={<Bookings />} />
+						<Route path="cabins" element={<Cabins />} />
+						<Route path="users" element={<Users />} />
+						<Route path="settings" element={<Settings />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
