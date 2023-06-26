d7de7d6abcad8485c739a2bd3fc2d6c16973665a
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-26 16:35:10 +1000

    Add CabinTable to component

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,5 +1,17 @@
+import CabinTable from '../features/cabins/CabinTable';
 import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 
 export default function Cabins() {
-	return <Heading as="h1">Cabins</Heading>;
+	return (
+		<>
+			<Row type="horizontal">
+				<Heading as="h1">All Cabins</Heading>
+				<p>Filter / Sort</p>
+			</Row>
+			<Row>
+				<CabinTable />
+			</Row>
+		</>
+	);
 }
