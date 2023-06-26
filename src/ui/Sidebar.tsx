3c3e5a0e7be96842cdc0a68bcd260f14c04a3590
kazblacktopp <karen.blacktopp@gmail.com>
2023-06-26 11:21:42 +1000

    Add MainNav component

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Logo from './Logo';
+import MainNav from './MainNav';
 
 const StyledSidebar = styled.aside`
 	background-color: var(--color-grey-0);
@@ -13,6 +14,7 @@ export default function Sidebar() {
 	return (
 		<StyledSidebar>
 			<Logo />
+			<MainNav />
 		</StyledSidebar>
 	);
 }
